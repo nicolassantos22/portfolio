edbdcbb2c5fd66b4d41955016235fdbe9b87b973
nicolassantos22 <nicolassantoss2001@gmail.com>
2025-03-04 15:39:28 -0300

    Atualização do mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const menuIcon = document.getElementById('menu-icon');
+const nav = document.getElementById('nav');
+const navOverlay = document.getElementById('nav-overlay');
+
+// Abrir o menu
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    navOverlay.classList.toggle('active');
+});
+
+// Fechar o menu ao clicar no overlay
+navOverlay.addEventListener('click', () => {
+    nav.classList.remove('active');
+    navOverlay.classList.remove('active');
+});
+
+
 const portfolioBoxes = document.querySelectorAll('.portfolio-box');
 const closeModalButtons = document.querySelectorAll('.close-modal');
 const modals = document.querySelectorAll('.modal');
